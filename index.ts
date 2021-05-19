88fb8dc21984819303d42109db883cc60a0f2bea
Romain Lanz <romain.lanz+github@pm.me>
2021-05-20 00:59:04 +0200

    refactor: correct bLuna->Luna swap, use decimaljs | SWAP NOT WORKING

--- index.ts
+++ index.ts
@@ -1,11 +1,25 @@
 require('dotenv').config();
+import Decimal from 'decimal.js';
 import { Coin, LCDClient, MnemonicKey, Msg, MsgExecuteContract } from '@terra-money/terra.js';
 
 const MICRO_MULTIPLIER = 1000000;
+
 const MINIMUM_REVERSE_SWAP_RATE = Number(process.env.MINIMUM_REVERSE_SWAP_RATE);
 const MINIMUM_SWAP_RATE = Number(process.env.MINIMUM_SWAP_RATE);
 const PAIR_TOKEN_ADDRESS = process.env.PAIR_TOKEN_ADDRESS;
 const BLUNA_TOKEN_ADDRESS = process.env.BLUNA_TOKEN_ADDRESS;
+
+type SimulationReturnType = {
+	return_amount: string;
+	spread_amount: string;
+	commission_amount: string;
+};
+
+type SimulationReturnTypeNormalized = {
+	returnAmount: Decimal;
+	spreadAmount: Decimal;
+	commissionAmount: Decimal;
+};
 
 const key = new MnemonicKey({
 	mnemonic: process.env.KEY,
@@ -14,47 +28,72 @@ const key = new MnemonicKey({
 const terra = new LCDClient({
 	URL: process.env.LCD_URL,
 	chainID: process.env.CHAIN_ID,
-	gasPrices: { uluna: 0.015 },
+	gasPrices: { uluna: 0.15 },
+	gasAdjustment: 1.05,
 });
 
 const wallet = terra.wallet(key);
 
-function getSwapRate(amount: number): any {
-	return terra.wasm.contractQuery(PAIR_TOKEN_ADDRESS, {
+async function getSimulationRate(amount: Decimal = new Decimal(100)) {
+	const rate = await terra.wasm.contractQuery<SimulationReturnType>(PAIR_TOKEN_ADDRESS, {
 		simulation: {
 			offer_asset: {
-				amount: String(amount * MICRO_MULTIPLIER),
+				amount: amount.times(MICRO_MULTIPLIER).toFixed(),
 				info: { native_token: { denom: 'uluna' } },
 			},
 		},
 	});
+
+	return {
+		returnAmount: new Decimal(rate.return_amount),
+		spreadAmount: new Decimal(rate.spread_amount),
+		commissionAmount: new Decimal(rate.commission_amount),
+	};
 }
 
-async function computePercentage({ return_amount }, amount = 100) {
-	const bLunaPrice = return_amount / MICRO_MULTIPLIER;
-	return ((bLunaPrice - amount) / amount) * 100;
+async function getReverseSimulationRate(amount: Decimal = new Decimal(100)) {
+	const rate = await terra.wasm.contractQuery<SimulationReturnType>(PAIR_TOKEN_ADDRESS, {
+		simulation: {
+			offer_asset: {
+				amount: amount.times(MICRO_MULTIPLIER).toFixed(),
+				info: { token: { contract_addr: BLUNA_TOKEN_ADDRESS } },
+			},
+		},
+	});
+
+	return {
+		returnAmount: new Decimal(rate.return_amount),
+		spreadAmount: new Decimal(rate.spread_amount),
+		commissionAmount: new Decimal(rate.commission_amount),
+	};
+}
+
+function computePercentage(rate: SimulationReturnTypeNormalized, amount: number) {
+	const bLunaPrice = rate.returnAmount.dividedBy(MICRO_MULTIPLIER);
+	return bLunaPrice.minus(amount).dividedBy(amount).times(100).toNumber();
 }
 
 async function displayBalance() {
 	const native = await terra.bank.balance(wallet.key.accAddress);
 	const bLuna = await getBLunaBalance();
 
-	console.log(
-		`New Balance: ${Number(native.get('uluna').amount) / MICRO_MULTIPLIER} Luna - ${bLuna / MICRO_MULTIPLIER} bLuna`
-	);
+	const lunaAmount = native.get('uluna').amount.dividedBy(MICRO_MULTIPLIER).toFixed(3);
+	const bLunaAmount = bLuna.amount.dividedBy(MICRO_MULTIPLIER).toFixed(3);
+
+	console.log(`New Balance: ${lunaAmount} Luna - ${bLunaAmount} bLuna`);
 }
 
 function getWalletBalance() {
 	return terra.bank.balance(wallet.key.accAddress);
 }
 
-function increaseAllowanceMessageFactory(amount: number) {
+function increaseAllowanceMessageFactory(amount: Decimal) {
 	return new MsgExecuteContract(
 		wallet.key.accAddress,
 		BLUNA_TOKEN_ADDRESS,
 		{
 			increase_allowance: {
-				amount: String(amount * MICRO_MULTIPLIER),
+				amount: amount.toFixed(),
 				spender: PAIR_TOKEN_ADDRESS,
 			},
 		},
@@ -62,24 +101,17 @@ function increaseAllowanceMessageFactory(amount: number) {
 	);
 }
 
-function swapBlunaToLunaMessageFactory(amount: number) {
-	return new MsgExecuteContract(
-		wallet.key.accAddress,
-		BLUNA_TOKEN_ADDRESS,
-		{
-			send: {
-				offer_asset: {
-					amount: String(amount * MICRO_MULTIPLIER),
-					contract: PAIR_TOKEN_ADDRESS,
-					msg: Buffer.from('{"swap":{}}').toString('base64'),
-				},
-			},
+function swapBlunaToLunaMessageFactory(amount: Decimal) {
+	return new MsgExecuteContract(wallet.key.accAddress, BLUNA_TOKEN_ADDRESS, {
+		send: {
+			amount: amount.times(MICRO_MULTIPLIER).toFixed(),
+			contract: PAIR_TOKEN_ADDRESS,
+			msg: 'eyJzd2FwIjp7fX0=',
 		},
-		[]
-	);
+	});
 }
 
-function swapLunaToBlunaMessageFactory(amount: number) {
+function swapLunaToBlunaMessageFactory(amount: Decimal) {
 	return new MsgExecuteContract(
 		wallet.key.accAddress,
 		PAIR_TOKEN_ADDRESS,
@@ -87,11 +119,11 @@ function swapLunaToBlunaMessageFactory(amount: number) {
 			swap: {
 				offer_asset: {
 					info: { native_token: { denom: 'uluna' } },
-					amount: String(amount * MICRO_MULTIPLIER),
+					amount: amount.times(MICRO_MULTIPLIER).toFixed(),
 				},
 			},
 		},
-		[new Coin('uluna', amount * MICRO_MULTIPLIER)]
+		[new Coin('uluna', amount.times(MICRO_MULTIPLIER).toFixed())]
 	);
 }
 
@@ -100,7 +132,7 @@ async function getBLunaBalance() {
 		balance: { address: wallet.key.accAddress },
 	});
 
-	return balance;
+	return new Coin('ubluna', balance);
 }
 
 function createAndSignTx(msgs: Msg[]) {
@@ -109,23 +141,23 @@ function createAndSignTx(msgs: Msg[]) {
 
 async function main() {
 	try {
-		const rate = await getSwapRate(100);
-		const percentage = await computePercentage(rate, 100);
+		const rate = await getSimulationRate();
+		const percentage = computePercentage(rate, 100);
 
 		console.log('-----------------------------------');
-		console.info(`Current Swap Percentage: ${percentage}%`);
+		console.info(`Current Swap Percentage: ${percentage.toFixed(3)}%`);
 
 		if (percentage < MINIMUM_REVERSE_SWAP_RATE) {
-			const blunaAmount = (await getBLunaBalance()) / MICRO_MULTIPLIER;
+			const blunaAmount = await getBLunaBalance();
+			const convertedAmount = blunaAmount.amount.dividedBy(MICRO_MULTIPLIER).toNumber();
 
-			if (Number(blunaAmount) > 2) {
-				console.info(`Swapping bLuna -> Luna [${blunaAmount} bLuna]`);
+			if (convertedAmount > 2) {
+				const toConvert = blunaAmount.amount.minus(100).dividedBy(MICRO_MULTIPLIER);
+				console.info(`Swapping bLuna -> Luna [${toConvert.toFixed(3)} bLuna]`);
 
-				const swapMessage = swapBlunaToLunaMessageFactory(blunaAmount);
-
+				const swapMessage = swapBlunaToLunaMessageFactory(toConvert);
 				const txs = await createAndSignTx([swapMessage]);
-				const r = await terra.tx.broadcast(txs);
-				console.log(r);
+				await terra.tx.broadcast(txs);
 				await displayBalance();
 			} else {
 				console.log('Not enough bLuna to swap');
@@ -134,26 +166,19 @@ async function main() {
 
 		if (percentage > MINIMUM_SWAP_RATE) {
 			const balance = await getWalletBalance();
-			let lunaAmount = Number(balance.get('uluna').amount) / MICRO_MULTIPLIER;
+			const lunaAmount = balance.get('uluna').amount;
 
-			const rate = await getSwapRate(lunaAmount);
-			const percentage = await computePercentage(rate, lunaAmount);
-			lunaAmount = Number((lunaAmount - (rate.commission_amount + 10) / MICRO_MULTIPLIER).toFixed(3));
+			const simulationRate = await getSimulationRate(lunaAmount.dividedBy(MICRO_MULTIPLIER));
+			const allowance = simulationRate.returnAmount.plus(10);
+			const toConvert = lunaAmount.minus(200).dividedBy(MICRO_MULTIPLIER);
 
-			if (lunaAmount > 2) {
-				if (percentage < MINIMUM_SWAP_RATE) {
-					console.log('Percentage changed!');
-					return;
-				}
+			if (toConvert.toNumber() > 2) {
+				console.info(`Swapping Luna -> bLuna [${toConvert.toFixed(3)} Luna]`);
 
-				console.info(`Swapping Luna -> bLuna [${lunaAmount} Luna]`);
-
-				const increaseAllowance = increaseAllowanceMessageFactory(rate.return_amount + 10);
-				const swapMessage = swapLunaToBlunaMessageFactory(lunaAmount);
-
+				const increaseAllowance = increaseAllowanceMessageFactory(allowance);
+				const swapMessage = swapLunaToBlunaMessageFactory(toConvert);
 				const txs = await createAndSignTx([increaseAllowance, swapMessage]);
-				const r = await terra.tx.broadcast(txs);
-				console.log(r);
+				await terra.tx.broadcast(txs);
 				await displayBalance();
 			} else {
 				console.log('Not enough Luna to swap');
@@ -163,7 +188,7 @@ async function main() {
 		console.error(e.response.data);
 	}
 
-	setTimeout(main, 1000);
+	setTimeout(main, 5000);
 }
 
 main();
